c9fca399a540a1f27c64f0f056f2ce9f69dcdedd
Justin <lam46@purdue.edu>
2024-10-24 00:48:56 -0700

    rounded corners, updated color and background images

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -3,18 +3,14 @@ import "./index.css";
 function Home() {
   return (
     <div className="home-container">
-      <span className="home-message">
-        <span className="home-welcome-name">
-          Hi, I'm Justin Lam!
-        </span>
-        <br />
-        I'm from the Bay Area and graduated with a degree in Computer Science
-        from Purdue University. Recently, I completed my master's at Johns
-        Hopkins University, where I specialized in Enterprise and Web Computing.
-        I'm passionate about growing my software development skills and working
-        on meaningful projects. In my free time, I enjoy swimming, cooking, and
-        traveling.
-      </span>
+      <span className="home-welcome-name">Hi, I'm Justin Lam!</span>
+      <br />
+      I'm from the Bay Area, California, and hold a Bachelor's degree in
+      Computer Science from Purdue University. Recently, I completed my Master's
+      in Computer Science at Johns Hopkins University, specializing in
+      Enterprise and Web Computing. I'm passionate about enhancing my software
+      development skills and contributing to impactful projects. In my free
+      time, I enjoy swimming, cooking, and exploring new travel destinations.
     </div>
   );
 }
